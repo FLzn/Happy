af0ac25822e1efe649b811ad9ac2c8af3b120766
Rafael Gonçalves <rafagpoliveira@gmail.com>
2020-10-16 10:27:33 -0300

    correção no database e finalização CreateOrphanage

--- web/src/pages/CreateOrphanage.tsx
+++ web/src/pages/CreateOrphanage.tsx
@@ -7,9 +7,13 @@ import { FiPlus } from "react-icons/fi";
 import '../styles/pages/create-orphanage.css';
 import Sidebar from "../components/Sidebar";
 import mapIcon from '../utils/mapIcon'
+import api from "../services/api";
+import { useHistory } from "react-router-dom";
 
-export default function CreateOrphanage() {
+export default function OrphanagesMap() {
+  
   const [position, setPosition] = useState({ latitude: 0, longitude: 0 })
+  const history = useHistory()
 
   const [name, setName] = useState('')
   const [about, setAbout] = useState('')
@@ -44,20 +48,32 @@ export default function CreateOrphanage() {
     setPreviewImages(selectedImagesPreview)
   }
 
-  function handleSubmit(event: FormEvent) {
+  async function handleSubmit(event: FormEvent) {
+    
+
     event.preventDefault()
 
     const { latitude, longitude } = position
 
-    console.log({
-      name,
-      about,
-      latitude,
-      longitude,
-      instructions,
-      opening_hours,
-      open_on_weekends
+    const data = new FormData()
+
+    data.append('name', name)
+    data.append('about', about)
+    data.append('latitude', String(latitude))
+    data.append('longitude', String(longitude))
+    data.append('instructions', instructions)
+    data.append('opening_hours', opening_hours)
+    data.append('open_on_weekends', String(open_on_weekends))
+
+    images.forEach(image => {
+      data.append('images', image)
     })
+
+    await api.post('orphanages', data)
+
+    alert('Cadastro realizado com sucesso!')
+
+    history.push('/app')
   }
 
   return (
